dc8e9e69c0aca344fa03b8ac95c0c2d6d8b69806
Farinesse <Farinesse>
2025-02-14 12:05:34 +0000

    new viz

--- data_viz.py
+++ data_viz.py
@@ -12,11 +12,15 @@ spark.read.format("delta").table("default.immobilier_analysis").count()
 
 # COMMAND ----------
 
-df = spark.read.format("delta").table("default.immobilier_analysis_1M")
+df = spark.read.format("delta").table("default.immobilier_analysis")
 
 # COMMAND ----------
 
-df=df.toPandas()
+df = df.sample(fraction=0.007)
+
+# COMMAND ----------
+
+df = df.toPandas()
 
 # COMMAND ----------
 
@@ -32,8 +36,12 @@ cols_to_convert = [
 ]
 df[cols_to_convert] = df[cols_to_convert].apply(pd.to_numeric, errors='coerce')
 
+# COMMAND ----------
+
 # Suppression des valeurs nulles après conversion
 df_clean = df.dropna()
+
+# COMMAND ----------
 
 # Filtrage des valeurs aberrantes (1er au 99e percentile)
 def filter_outliers(df, column):
@@ -52,11 +60,17 @@ def plot_histogram(data, column, bins=50, color='blue', xlabel='', title=''):
     plt.grid(True)
     plt.show()
 
+# COMMAND ----------
+
 # Histogramme des prix des biens
 plot_histogram(df_filtered, "prix", xlabel="Prix (€)", title="Distribution des prix des biens immobiliers")
 
+# COMMAND ----------
+
 # Histogramme des surfaces habitables
 plot_histogram(df_filtered, "surface_habitable", color='green', xlabel="Surface Habitable (m²)", title="Distribution des surfaces habitables")
+
+# COMMAND ----------
 
 # Évolution du prix moyen par année
 prix_par_annee = df_clean.groupby("annee")["prix"].mean()
@@ -68,6 +82,7 @@ plt.title("Évolution du prix moyen des biens immobiliers par année")
 plt.grid(True)
 plt.show()
 
+# COMMAND ----------
 
 # Relation Surface habitable vs Prix
 plt.figure(figsize=(10, 5))
@@ -78,6 +93,8 @@ plt.ticklabel_format(style='plain', axis='y')  # Affichage du prix réel
 plt.title("Relation entre la surface habitable et le prix des biens")
 plt.grid(True)
 plt.show()
+
+# COMMAND ----------
 
 # Relation Surface habitable vs Prix bettween 0 and 20 millions
 plt.figure(figsize=(10, 5))
@@ -90,6 +107,8 @@ plt.ylim(0, 20000000)  # Fixer l'ordonnée de 0 à 60 millions
 plt.grid(True)
 plt.show()
 
+# COMMAND ----------
+
 # Prix moyen des biens par département (Top 10)
 prix_par_departement = df_clean.groupby("departement")["prix"].mean().nlargest(10)
 plt.figure(figsize=(10, 5))
@@ -100,12 +119,16 @@ plt.title("Top 10 des départements avec les prix moyens les plus élevés")
 plt.xticks(rotation=45)
 plt.show()
 
+# COMMAND ----------
+
 # Heatmap des corrélations
 df_numeric = df.select_dtypes(include=[np.number])
 plt.figure(figsize=(12, 8))
 sns.heatmap(df_numeric.corr(), annot=True, fmt=".2f", cmap="coolwarm", linewidths=0.5)
 plt.title("Heatmap des Corrélations")
 plt.show()
+
+# COMMAND ----------
 
 # Répartition du nombre de pièces
 plt.figure(figsize=(10, 5))
@@ -116,10 +139,48 @@ plt.title("Répartition du nombre de pièces des biens immobiliers")
 plt.grid(True)
 plt.show()
 
+# COMMAND ----------
+
 # Distribution des loyers au m²
 for col, color, label in zip(["loyer_m2_maison", "loyer_m2_appartement"], ["blue", "green"], ["Maison", "Appartement"]):
     df_filtered = filter_outliers(df_clean, col)
-    plot_histogram(df_filtered, col, xlabel=f"Loyer au m² (€) - {label}", title=f"Distribution du loyer au m² des {label.lower()}s", color=color)
+    plot_histogram(df_filtered, col, xlabel=f"Loyer moyen mensuel au m² (€) - {label}", title=f"Distribution du loyer moyen menseuel au m² des {label.lower()}s", color=color)
+
+
+# COMMAND ----------
+
+!pip install folium
+
+# COMMAND ----------
+
+import folium
+
+def get_color(prix):
+    if prix < 100000:
+        return "green"
+    elif 100000 <= prix < 250000:
+        return "blue"
+    elif 250000 <= prix < 500000:
+        return "orange"
+    else:
+        return "red"
+
+pdf_filtered = df.dropna(subset=['latitude', 'longitude', 'prix'])
+
+map_france = folium.Map(location=[46.603354, 1.888334], zoom_start=6)
+
+for _, row in pdf_filtered.iterrows():
+    folium.CircleMarker(
+        location=[row["latitude"], row["longitude"]],
+        radius=5,
+        color=get_color(row["prix"]),
+        fill=True,
+        fill_color=get_color(row["prix"]),
+        fill_opacity=0.7,
+        popup=f"Prix: {row['prix']} €"
+    ).add_to(map_france)
+
+map_france
 
 
 # COMMAND ----------
